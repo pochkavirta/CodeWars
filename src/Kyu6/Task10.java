d2da4165bccbb9ef971e68c91043f867a06ff501
PochkaVirta <cyberdog771@gmail.com>
2025-06-25 20:27:02 +0300

    added new tasks

--- src/Kyu6/Task10.java
+++ src/Kyu6/Task10.java
@@ -1,6 +1,8 @@
 package Kyu6;
 
-//TODO
+//TODO The goal of this exercise is to convert a string to a new string where each character in the new string is
+// "(" if that character appears only once in the original string, or ")" if that character appears more than once
+// in the original string. Ignore capitalization when determining if a character is a duplicate.
 
 public class Task10 {
     public static void main(String[] args) {
