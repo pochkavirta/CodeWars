0c9f6f7e830c08e828636e08e3ec5c26c02a3403
PochkaVirta <cyberdog771@gmail.com>
2025-08-07 22:30:08 +0300

    added new tasks

--- src/Kyu6/Task18.java
+++ src/Kyu6/Task18.java
@@ -6,6 +6,7 @@ public class Task18 {
     public static void main(String[] args) {
         System.out.println("abc");
         System.out.println("abcdef");
+        System.out.println("HelloWorld");
     }
 
     public static String[] solution(String s) {
