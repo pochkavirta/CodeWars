d9102be476b3e25f8091b8849e7e0b546b4c7412
PochkaVirta <cyberdog771@gmail.com>
2025-08-11 15:27:22 +0300

    added new tasks

--- src/Kyu6/Task18.java
+++ src/Kyu6/Task18.java
@@ -1,6 +1,7 @@
 package Kyu6;
 
-//TODO
+//TODO Complete the solution so that it splits the string into pairs of two characters. If the string contains an odd
+// number of characters then it should replace the missing second character of the final pair with an underscore ('_').
 
 public class Task18 {
     public static void main(String[] args) {
