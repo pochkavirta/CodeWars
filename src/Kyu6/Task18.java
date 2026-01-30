3d0524ca2c375e53f3b3fc870980a33b4302f9f5
PochkaVirta <cyberdog771@gmail.com>
2026-01-30 04:53:21 +0300

    added new tasks

--- src/Kyu6/Task18.java
+++ src/Kyu6/Task18.java
@@ -3,15 +3,23 @@ package Kyu6;
 //TODO Complete the solution so that it splits the string into pairs of two characters. If the string contains an odd
 // number of characters then it should replace the missing second character of the final pair with an underscore ('_').
 
+import java.util.Arrays;
+
 public class Task18 {
     public static void main(String[] args) {
-        System.out.println("abc");
-        System.out.println("abcdef");
-        System.out.println("HelloWorld");
-        System.out.println("abcde");
+        System.out.println(Arrays.toString(solution(null)));
+        System.out.println(Arrays.toString(solution("abc")));
+        System.out.println(Arrays.toString(solution("abcdef")));
+        System.out.println(Arrays.toString(solution("HelloWorld")));
+        System.out.println(Arrays.toString(solution("abcde")));
+        System.out.println(Arrays.toString(solution("LovePizza")));
     }
 
     public static String[] solution(String s) {
-        return null;
+        if (s == null || s.isEmpty()) return new String[0];
+        if (s.length() % 2 == 1) {
+            s = s + "_";
+        }
+        return s.split("(?<=\\G.{2})");
     }
 }
